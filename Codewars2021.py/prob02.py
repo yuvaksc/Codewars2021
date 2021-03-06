36c1ff89b5be40e37e8756bf60834b1168614df6
Yuva Komara <yuvaksc@gmail.com>
2021-03-06 09:36:10 -0800

    new commit

--- Codewars2021.py/prob02.py
+++ Codewars2021.py/prob02.py
@@ -1,3 +1,11 @@
-x = int(input())
-y = int(input())
-print(x * y)
+import math
+
+num = input().split(' ')
+r = math.pi * float(num[1]) * float(num[1]) * float(num[0])
+r = round(r, 2)
+sp = str(r).split('.')
+while len(sp[1]) < 2:
+    r = str(r) + '0'
+    if len(r.split('.')[1]) == 2:
+        break
+print(r, 'cubic inches')
--- Codewars2021.py/prob05.py
+++ Codewars2021.py/prob05.py
@@ -1,14 +1,31 @@
-A = input()
-B = input()
-C = input()
+x = int(input())
+nums = []
+for i in range(x):
+    d = input().split('/')
+    nums.append(d)
+print(nums)
+for num in nums:
+    date = num[0] + num[1]
+    del num[0]
+    del num[1]
+    num.insert(0, date)
+print(nums)
+counts = []
+for day in nums:
+    count = 0
+    for d in nums:
+        if day[0] == d[0]:
+            count += 1
+    counts.append(count)
+print(counts)
 
-print(A, "* (" + B, "+", C + ") =", A, "*", B, "+", A, "*", C)
-c1 = int(B) + int(C)
-c2 = int(A) * int(B)
-c3 = int(A) * int(C)
+result = []
+for i, c in enumerate(counts):
+    if c > 1:
+        x = nums[i][0]
+        if x not in result:
+            result.append(x)
 
-print(A, "*", str(c1), "=", str(c2), "+", str(c3))
-c4 = int(A) * c1
-c5 = c2 + c3
-print(c4, "=", c5)
+print(len(result), 'duplicates:', i for i in result)
+
 
