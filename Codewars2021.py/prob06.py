956a792cd31fc53a29559d9c0f293588a31868dc
Yuva Komara <yuvaksc@gmail.com>
2021-03-06 11:11:26 -0800

    commit

--- Codewars2021.py/prob06.py
+++ Codewars2021.py/prob06.py
@@ -1,3 +1,4 @@
+"""
 num = (input())
 words = input().split()
 
@@ -10,3 +11,26 @@ for i in words:
 for i in words:
     print(i)
 print(result)
+"""
+num = (input())
+words = input().split()
+print(words)
+
+result = []
+for i in words:
+    for letter in i:
+        e = ord(str(letter))
+        d = chr(e - 5)
+        if d == '=': 
+            d = chr(e + 21)
+        result.append(d)
+
+def convert(result):
+    new = ''
+    for x in result: 
+        new +=x
+    return new
+    
+w = len(words[0])
+x = convert(result)
+print(x[0:w], x[w:])
